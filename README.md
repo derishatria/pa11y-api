74716ad8f2a8a48c06c3ff9db5b9be994f5e92b4
derishatria <derishatria92@gmail.com>
2021-12-07 16:16:02 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# pa11y-api
-Backend API pa11y (Website Accessibility Tester)
+<h1 align="center">Pa11y Api</h1>
+
+<h2 align="center">Website Accessibility Tester</h2>
+
+-----------------------------------
+<p align="center"><a href="https://github.com/pa11y/pa11y">pa11y</a> is your automated accessibility testing pal. It runs accessibility tests on your pages via the command line or Node.js, so you can automate your testing process.</p>
+
+## Requirements 
+- [Docker 20.05 or higher](https://docs.docker.com/get-docker/) 
+- [NodeJS v12 or higher](https://nodejs.org/en/) 
+- npm >= 6.14.15
---
+++ pa11y.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const pa11y = require('pa11y')
+const puppeteer = require('puppeteer')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/pa11y', async (req, res) => {
+  if (!req.query.url) {
+    res.status(400).json({ error: 'url is required' })
+  } else {
+    const browser = await puppeteer.launch({
+      ignoreHTTPSErrors: true,
+      args: [
+        '--disable-dev-shm-usage',
+        '--no-sandbox',
+        '--incognito'
+      ],
+    });
+    const results = await pa11y(req.query.url, {
+      browser: browser
+    });
+    browser.close();
+    res.status(200).json(results)
+  }
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
